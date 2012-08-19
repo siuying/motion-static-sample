39455bd0338684ab6dfe722ff8c218986e040016
Francis Chong <francis@ignition.hk>
2012-08-19 16:16:40 +0800

    use instance variable in the counter

--- app/motion_app_delegate.rb
+++ app/motion_app_delegate.rb
@@ -11,11 +11,11 @@ class MotionAppDelegate
   end
 
   def setup_timer
-    count = 0
+    @count = 0
     @timer = EM.add_periodic_timer 1.0 do
-      count = count + 1
-      puts "Hi (#{count})!"
-      (count < 10) || EM.cancel_timer(@timer)
+      @count = @count + 1
+      puts "Hi (#{@count})!"
+      (@count < 10) || EM.cancel_timer(@timer)
     end
   end
 end
